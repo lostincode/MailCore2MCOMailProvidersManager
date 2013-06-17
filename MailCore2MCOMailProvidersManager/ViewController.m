e35849c51de308b8ac44d4d67b715bd14d45ea44
Bill <roundedvision@gmail.com>
2013-06-17 13:59:11 -0400

    added example for sending

--- MailCore2MCOMailProvidersManager/ViewController.m
+++ MailCore2MCOMailProvidersManager/ViewController.m
@@ -22,6 +22,42 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)sendMailAction:(id)sender {
+
+    MCOSMTPSession *smtpSession = [[MCOSMTPSession alloc] init];
+    smtpSession.hostname = @"";
+    smtpSession.port = 587;
+    smtpSession.username = @"";
+    smtpSession.password = @"";
+    smtpSession.authType = (MCOAuthTypeSASLPlain | MCOAuthTypeSASLLogin);
+    smtpSession.connectionType = MCOConnectionTypeClear;
+    
+    //build the message
+    MCOMessageBuilder * builder = [[MCOMessageBuilder alloc] init];
+
+    //set from
+    [[builder header] setFrom:[MCOAddress addressWithDisplayName:@"Test" mailbox:@""]];
+    
+    //set to: recipients
+    [[builder header] setTo:@[[MCOAddress addressWithMailbox:@""]]];
+    
+    //set subject
+    [[builder header] setSubject:@"SUBJECT HERE"];
+    
+    //crashes if you send @""
+    NSString *body = @"This is a test";
+    NSString *htmlBody = [NSString stringWithFormat:@"<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\"></head><body>%@</body></html>", body];
+    [builder setHTMLBody:htmlBody];
+    
+    
+    NSData * rfc822Data = [builder data];
+    MCOSMTPSendOperation *sendOperation = [smtpSession sendOperationWithData:rfc822Data];
+    
+    //start the operation
+    [sendOperation start:^(NSError *error) {
+        NSLog(@"error:%@", [error localizedDescription]);
+    }];
+}
 
 
 - (IBAction)buttonPressed:(id)sender {
